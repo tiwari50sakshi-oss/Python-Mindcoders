e0f10a8288fd7b113de2bd65b40ad374c1571a5d
sakshii321 <sakshi50tiwari@gmail.com>
2026-05-26 05:37:04 -0700

    Scope

--- FunctionScope.py
+++ FunctionScope.py
@@ -83,14 +83,6 @@ def factorial(number):
 print("Factorial of an number:",factorial(5))
 
 
-my_tuple=(1,10,100)
-t1=my_tuple + (1000,10000)
-t2=my_tuple * 3
 
-print(len(t2))
-print(t1)
-print(t2)
-print(10 in my_tuple)
-print(-10 not in my_tuple)
 
 
